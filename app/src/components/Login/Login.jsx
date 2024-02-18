7b8bb336bda9bf2f9d8ca48cf10955e2fc4930a4
Surya Prakash Maurya <43824111+spm999@users.noreply.github.com>
2024-02-18 23:38:25 +0530

    Update Login.jsx

--- app/src/components/Login/Login.jsx
+++ app/src/components/Login/Login.jsx
@@ -21,19 +21,20 @@ const Login = () => {
         email,
         password
       });
-      // console.log(res.data); // Assuming you want to do something with the response, like redirecting the user
       // Save token to localStorage
-      const userId = res.data.UserId;
-      // console.log(userId)
       localStorage.setItem('authToken', res.data.authtoken);
       // Redirect to Dashboard or wherever you want after successful login
+      const userId = res.data.UserId;
       navigate(`/${userId}`);
     } catch (error) {
-      console.error('Login Error:', error.message);
-            setError(error)
-
+      if (error.response && error.response.status === 404) {
+        setError('Email is not registered');
+      } else {
+        setError('An error occurred. Please try again later.');
+      }
     }
   };
+
 
   return (
     <div className="home-container">
@@ -73,7 +74,7 @@ const Login = () => {
         </div>
         <input type="submit" value="Login" className="form-submit" />
       </form>
-              {error}
+        {error && <p className="error-message">{error}</p>}
 
       </div>
       <p className="signup-text">Don't have an account? <Link to="/register" className="signup-link">Sign Up</Link></p>
